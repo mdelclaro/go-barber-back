e7b45720f5edef49a667a8b400d8c528aae6e7e9
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-08-23 16:42:35 -0300

    listing notifications to providers

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/File';
 import ProviderController from './app/controllers/Provider';
 import AppointmentController from './app/controllers/Appointment';
 import ScheduleController from './app/controllers/Schedule';
+import NotificationController from './app/controllers/Notification';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -28,6 +29,8 @@ routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
