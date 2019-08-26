39e49133961bc2e185c842f16529c12bebe60b2e
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-08-25 23:09:20 -0300

    fix

--- src/app/controllers/User.js
+++ src/app/controllers/User.js
@@ -67,6 +67,8 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
+    await user.update(req.body);
+
     const { id, name, avatar } = await User.findByPk(req.userId, {
       include: [
         {
@@ -76,6 +78,7 @@ class UserController {
         },
       ],
     });
+
     return res.json({ id, name, email, avatar });
   }
 }
